a546da309e6cd358b7c4e2e57917fa1db1af4e8c
Thanh Tu <thanhtu10803@gmail.com>
2025-09-24 13:45:10 +0700

    Feat(main): Add data structures for header and weather records

--- project2_thanhtu/main.c
+++ project2_thanhtu/main.c
@@ -9,7 +9,7 @@
  *      INCLUDES
  *********************/
 #include <Stdio.h>
-
+#include <stdint.h>
 /*********************
  *      DEFINES
  *********************/
@@ -17,6 +17,36 @@
 /**********************
  *      TYPEDEFS
  **********************/
+typedef struct
+{
+    uint32_t id_file;
+    uint32_t number_record;
+    uint16_t version;
+} s_header_t;
+// 12byte (4 4 2 + 2padding)
+
+typedef struct
+{
+    //8byte
+    double lati;
+    double longi;
+    //4 byte
+    uint32_t sensor_id;
+    uint32_t timestamp;
+    float rain_fall;
+    float uv;
+    float light;
+    float temp;
+    float hump;
+    float press;
+    float wind_speed;
+    //2 byte
+    uint16_t co2;
+    uint16_t wind_direction;
+    //1 byte
+    uint8_t baterry;
+} s_weatherRecord_t;
+//64byte: 57+1+7padding
 
 /**********************
  *  STATIC PROTOTYPES
@@ -35,7 +65,6 @@
  **********************/
 int main()
 {
-
     return 0;
 }
 /**********************
